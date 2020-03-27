9cb25d855cff3f20208f69cad7d6698e0fcd62f9
Ashley Scott <aescott87@gmail.com>
2020-03-27 18:32:03 -0500

    Set up index.html file, created database, created necessary files

---
+++ weekend_to_do_app.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "tasks"(
+    "id" serial PRIMARY KEY,
+    "task" varchar(100) NOT NULL,
+    "due_date" date,
+    "task_complete" varchar(10)
+);
+
+INSERT INTO "tasks"("task", "due_date", "task_complete")
+VALUES ('Sweep kitchen floor', '3/29/2020', 'Incomplete'),
+('Finish Prime homework', '3/29/2020', 'Incomplete'),
+('Pay phone bill', '4/10/2020', 'Incomplete');
